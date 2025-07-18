64802f2a77fdc424ee23293d95c3f898bab61f27
Chetan Tekam <tekamchetan3500@gmail.com>
2025-07-18 12:18:38 +0530

    fix: pydantic change to pydantic_settings for import BaseSettings

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,12 @@
 # Handles environment variables and configuration
 
-from pydantic import BaseSetting
+from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-class Settings(BaseSetting):
+class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://username:password@localhost:5432/fastapi_db")
     PROJECT_NAME: str = "FastAPI PostgreSQL CRUD"
     API_V1_STR: str = "/api/v1"
