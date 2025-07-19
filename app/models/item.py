7d3519703e0b7ca6825c015992d3419c8da7520a
Chetan Tekam <tekamchetan3500@gmail.com>
2025-07-19 09:40:28 +0530

    refactor: model.py

--- app/models/item.py
+++ app/models/item.py
@@ -1,12 +1,17 @@
-# Define the Pydantic model for request/respond validation
+"""Define the Pydantic model for request/respond validation"""
 
 from pydantic import BaseModel
 
+
 class Item(BaseModel):
+    """Model"""
+
     id: int
     name: str
     price: float
     is_available: bool = True
 
     class Config:
+        """Model Config"""
+
         from_attributes = True
